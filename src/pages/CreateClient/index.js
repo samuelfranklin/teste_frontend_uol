064d47f6f2830308568c707f13cd2f09c22e309b
Samuel Franklin Venturin <samuelfranklinsj@gmail.com>
2022-04-01 17:03:06 -0300

    teste UOL: Painel de Clientes

---
+++ src/pages/CreateClient/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header, Form } from "../../components";
+
+import "./style.css";
+
+export default function CreateClient() {
+  return (
+    <div id="create-client">
+      <Header title="Clientes">
+        <h3>Adicionar Cliente</h3>
+      </Header>
+      <Form formType="create" />
+    </div>
+  );
+}
---
+++ src/pages/UpdateClient/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Header, Form } from "../../components";
+import { useParams } from "react-router-dom";
+import { getClient } from "../../services/clients";
+
+import "./style.css";
+
+export default function UpdateClient() {
+  const { id } = useParams();
+  let selectedClient = null;
+
+  try {
+    selectedClient = getClient(id);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return (
+    <div id="update-client">
+      <Header title="Clientes">
+        <h3>Editar Cliente</h3>
+      </Header>
+      <Form client={selectedClient} formType="update" />
+    </div>
+  );
+}
